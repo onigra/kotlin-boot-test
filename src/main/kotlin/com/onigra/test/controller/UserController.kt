a272bb65e7b60669a28b875c7024e6a9e6ba776e
onigra <3280467rec@gmail.com>
2019-02-13 00:17:35 +0900

    data classからレスポンスを生成

--- src/main/kotlin/com/onigra/test/controller/UserController.kt
+++ src/main/kotlin/com/onigra/test/controller/UserController.kt
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import com.onigra.test.model.User
+import org.springframework.http.MediaType
 
 @RestController
 @RequestMapping("users")
 class UserController {
-    @GetMapping("{id}")
+    @GetMapping("{id}", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun show(@PathVariable("id") id: Int): String {
-        return id.toString()
+        val user = User("yudai", id)
+        return "{ \"name\": \"${user.name}\", \"age\": \"${user.age}\" }"
     }
 }
---
+++ src/main/kotlin/com/onigra/test/model/User.kt
@@ -0,0 +1,3 @@
+package com.onigra.test.model
+
+data class User(val name: String, val age: Int)
